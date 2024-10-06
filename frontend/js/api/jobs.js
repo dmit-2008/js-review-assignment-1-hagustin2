1bc909e58a733e38e06e9db4cef55b9eb6959e00
hagustin2 <156492298+hagustin2@users.noreply.github.com>
2024-10-05 23:39:14 -0600

    Update jobs.js

--- frontend/js/api/jobs.js
+++ frontend/js/api/jobs.js
@@ -1,1 +1,34 @@
 // your code goes here.
+const BASE_URL = 'http://localhost:3000/jobs';
+
+const searchJobs = async (query) => {
+    return await fetch(`${BASE_URL}?q=${query}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(`${BASE_URL}?q=${query}`);
+            return data;
+        })
+        .catch(error => console.log(error)); 
+
+   
+};
+
+export { searchJobs }
+
+  const fetchJobDetails =async(id)=> {
+    return await fetch(`${BASE_URL}/${id}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(`Fetched details for job ID ${id}`);
+            return data;
+        })
+        .catch(error => console.log(error)); 
+
+
+};
+
+export { searchJobs, fetchJobDetails };
